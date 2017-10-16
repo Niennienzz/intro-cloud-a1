9a8f4a40e3c5b581d4d2e66856f4deada6951e05
zhehui <zhehui.zhou@outlook.com>
2017-10-16 03:16:31 -0400

    welcome page

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,34 +1,84 @@
-function checkLoginForm() {
+var welcomePageApp = new Vue({
 
-    // get element values
-    let userName = document.getElementById('loginUsername').value;
-    let password = document.getElementById('loginPassword').value;
+    // select DOM element for Vue
+    el: '#welcomePageApp',
 
-    // simple validation
-    if (userName.length == 0 || password.length == 0) {
-        alert("Empty username or password.");
-        return;
-    }
+    // data to be keep in client memory
+    data: {
+        username: '',
+        password: '',
+        isOnRegisterTab: true,
+        userRegisterURL: '/user',
+        userLoginUserURL: '/auth'
+    },
 
-    // send async request to create user
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/user");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert("User created, redirecting to home page...");
-        }
-        else if (xhr.readyState == 4 && xhr.status == 400) {
-            let jsonResponse = JSON.parse(xhr.responseText);
-            if (jsonResponse.message == "user already exists") {
-                alert("User already exists. Please log in.");
+    // methods controlling the view
+    methods: {
+
+        switchToRegisterTab: function() {
+            this.isOnRegisterTab = true;
+            document.getElementById("switchToRegisterButton").disabled = this.isOnRegisterTab;
+            document.getElementById("switchToLoginButton").disabled = !this.isOnRegisterTab;
+        },
+
+        switchToLoginTab: function() {
+            this.isOnRegisterTab = false;
+            document.getElementById("switchToRegisterButton").disabled = this.isOnRegisterTab;
+            document.getElementById("switchToLoginButton").disabled = !this.isOnRegisterTab;
+        },
+
+        checkRegisterForm: function(type) {
+            let username = document.getElementById('registerUsername').value;
+            let password = document.getElementById('registerPassword').value;
+            if (username.length == 0 || password.length == 0) {
+                alert("Empty username or password.");
+                return;
             }
-        } else {
-            alert("An error occurred.")
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", this.userRegisterURL);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function(vm) {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    alert("User created, redirecting to home page...");
+                }
+                else if (xhr.readyState == 4 && xhr.status == 400) {
+                    let jsonResponse = JSON.parse(xhr.responseText);
+                    if (jsonResponse.message == "user already exists") {
+                        alert("User already exists, redirecting to home page...");
+                    }
+                    return;
+                }
+            }.bind(xhr, this)
+            xhr.send(JSON.stringify({username: username, password: password}));
+            return;
+        },
+
+        checkLoginForm: function() {
+            let username = document.getElementById('loginUsername').value;
+            let password = document.getElementById('loginPassword').value;
+            if (username.length == 0 || password.length == 0) {
+                alert("Empty username or password.");
+                return;
+            }
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", this.userLoginUserURL);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = function(vm) {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    alert("Redirecting to home page...");
+                }
+                else if (xhr.readyState == 4 && xhr.status == 401) {
+                    let jsonResponse = JSON.parse(xhr.responseText);
+                    if (jsonResponse.description == "Invalid credentials") {
+                        alert("Invalid username or password.");
+                    }
+                    return;
+                }
+            }.bind(xhr, this)
+            xhr.send(JSON.stringify({username: username, password: password}));
+            return;
         }
     }
-    xhr.send(JSON.stringify({username: userName, password: password}));
 
-    return;
+})
 
-}

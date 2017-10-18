16bc6444ac6be9bc682e041be8852f6ac778c9bd
zhehui <zhehui.zhou@outlook.com>
2017-10-18 00:14:40 -0400

    change host

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ api.add_resource(TestUploadResource, '/test/FileUpload')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)

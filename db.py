9d10a0a3b0eac1146b07c5265d934f53731213cb
Zhehui Zhou <zhehui.zhou@outlook.com>
2017-10-18 17:32:15 -0400

    db.py docstring

--- db.py
+++ db.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+""" Database instance.
+
+db is a default database instance of Flask-SQLAlchemy.
+It uses SQLite3 as the backing database.
+It is configured (in app.py) to save to the data.db file within app directory.
+"""

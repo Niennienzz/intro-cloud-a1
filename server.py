9d5858026503bc1fddfc27108ea7e0380d0b3198
zhehui <zhehui.zhou@outlook.com>
2017-11-07 21:35:59 -0500

    changes to acomodate gunicorn & gevent

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from datetime import timedelta
+from db import db
 from flask import Flask, session, request, render_template, redirect, url_for
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,28 +11,27 @@ from resources.pic_url import PicUploaderResource, PicURLListResource
 from resources.test import TestUploadResource
 
 
-""" Application initialization and configs.
-
-Flask is initialized for this application.
-Flask-SQLAlchemy uses MySQL server, and uses 'sys' as backing database.
-Flask-JWT tokens have expiration time of one day.
-"""
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/sys'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=86400)
-app.secret_key = 'An_App_Secret_Key'
-api = Api(app)
-
-
-# Ensure database tables are created.
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# Application initialization and configs.
+# Flask is initialized for this application.
+# Flask-SQLAlchemy uses MySQL server, and uses 'sys' as backing database.
+# Flask-JWT tokens have expiration time of one day.
+#
+def create_app():
+    ap = Flask(__name__)
+    ap.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/sys'
+    ap.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    ap.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=86400)
+    ap.secret_key = 'An_App_Secret_Key'
+    db.init_app(ap)
+    with ap.app_context():
+        db.create_all()
+    return ap
 
 
 # Flask-JWT authorization, which by default register the route
 # '/auth' for user authentication.
+app = create_app()
+api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
 
@@ -72,14 +72,9 @@ api.add_resource(PicUploaderResource, '/api/pic_upload')
 api.add_resource(PicURLListResource, '/api/pic_urls')
 api.add_resource(PicResource, '/api/pic/<path:file_path>')
 
-
-"""
->>>>> Test API endpoint, which is self explaining.  <<<<<
->>>>> See /resources/test.py file for more details. <<<<<
-"""
+# >>>>> Test API endpoint, which is self explaining.  <<<<<
+# >>>>> See /resources/test.py file for more details. <<<<<
 api.add_resource(TestUploadResource, '/test/FileUpload')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()

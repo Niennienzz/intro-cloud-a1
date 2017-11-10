fbc5c4c0a1bad905f4453182b34b4eb08e765819
zhehui <zhehui.zhou@outlook.com>
2017-11-10 04:04:25 -0500

    minor in constants

--- const/const.py
+++ const/const.py
@@ -1,26 +1,17 @@
 class Constants:
     """Constants provides access to useful constants.
-
-        Attributes:
-            ORIGIN (str): The filename of the original image.
-            THUMB (str): The filename of the thumbnail image.
-            TRANS_1 (str): The filename of the 1st transformation.
-            TRANS_2 (str): The filename of the 2nd transformation.
-            TRANS_3 (str): The filename of the 3rd transformation.
-            ALPHABET (str): The alphabet including 1-9, a-z, and A-Z for salt generation.
-            INSTANCES (slice of str): Pre-loaded instance IDs.
     """
-    # images
+    # Constants for image files.
     ORIGIN = 'origin.jpg'
     THUMB = 'thumbnail.jpg'
     TRANS_1 = 'trans1.jpg'
     TRANS_2 = 'trans2.jpg'
     TRANS_3 = 'trans3.jpg'
 
-    # hash
+    # Constant for SHA-256 hash salt.
     ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    # aws
+    # Constants for AWS related services.
     MANAGER_DATABASE_ID = 1
     USER_WORKER_IMAGE_ID = 'ami-fa6cd180'
     SECURITY_GROUP = 'ECE-1779'

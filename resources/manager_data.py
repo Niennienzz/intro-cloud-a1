3e4f617889223cd6e1c69b08b73fc71646b1b53b
zhehui <zhehui.zhou@outlook.com>
2017-11-12 17:57:57 -0500

    refactor purge data

--- resources/manager_data.py
+++ resources/manager_data.py
@@ -1,10 +1,10 @@
-import asyncio
+import boto3
+import botocore
 from const.const import Constants
 from flask_restful import Resource
 from flask_jwt import jwt_required, current_identity
 from models.pic_url import PicURLModel
 from models.user import UserModel
-from store.s3 import PicS3Store
 
 
 class ManagerData(Resource):
@@ -27,32 +27,10 @@ class ManagerData(Resource):
             return {'message': 'you are not manager'}, 403
 
         # get all picture urls
-        pic_url_models = PicURLModel.get_all_full()
-        urls = []
-        for pic_url_model in pic_url_models:
-            urls.append(pic_url_model.origin)
-            urls.append(pic_url_model.thumb)
-            urls.append(pic_url_model.trans1)
-            urls.append(pic_url_model.trans2)
-            urls.append(pic_url_model.trans3)
-
-        # make s3 store objects
-        s3_stores = []
-        for url in urls:
-            s3_stores.append(PicS3Store(url, None))
-
-        # get delete funcs
-        s3_delete_funcs = []
-        for store in s3_stores:
-            s3_delete_funcs.append(store.delete)
-
-        # run each delete func in a coroutine
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(asyncio.gather(
-            *(func() for func in s3_delete_funcs)
-        ))
-        loop.close()
+        bucket_name = 'zhehui-ece1779-bucket'
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+        bucket.objects.all().delete()
 
         # delete table entries for picurls
         pic_url_models = PicURLModel.get_all()
